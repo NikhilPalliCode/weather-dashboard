e3eaac8ee880821565d031b6a2727f9711896208
Nikhil <166955050+NikhilPalliCode@users.noreply.github.com>
2025-03-24 21:43:48 -0500

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { DecimalPipe } from '@angular/common';
+import { WeatherService } from './services/weather.service';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, FormsModule, DecimalPipe],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
+})
+
 export class AppComponent implements OnInit {
   weatherData: any = {
     current: {},
